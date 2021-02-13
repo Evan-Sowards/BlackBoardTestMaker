891280ef58a9478149dfe1708a6733dfa11fefd5
Evan-Sowards <46791498+Evan-Sowards@users.noreply.github.com>
2021-02-12 23:51:03 -0600

    Add files via upload
    Cleaned up existing code; implemented more functions in preparation of OOP stuff later; finished the function for creating test files

--- excelTest.py
+++ excelTest.py
@@ -1,86 +1,71 @@
 import openpyxl
-from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font, cell_style, Color
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Font
 from openpyxl.utils import get_column_letter
-wb = openpyxl.Workbook()
-filepath = "C:/Users/EvanS/Desktop/test.xlsx"
-wb.save(filepath)
-
-wb1 = openpyxl.load_workbook(filepath)
-
-mySheet = wb1['Sheet']
-
-mySheet['A1'] = 'Course Name'
-mySheet['B1'] = 'Test Name'
-mySheet['C1'] = 'Question Text'
-mySheet['D1'] = 'Right Answer 1'
-mySheet['E1'] = 'Right Answer 2'
-mySheet['F1'] = 'Right Answer 3'
-mySheet['G1'] = 'Right Answer 4'
-mySheet['H1'] = 'Right Answer 5'
-mySheet['I1'] = 'Right Answer 6'
-mySheet['J1'] = 'Right Answer 7'
-mySheet['K1'] = 'Right Answer 8'
-mySheet['L1'] = 'Wrong Answer 1'
-mySheet['M1'] = 'Wrong Answer 2'
-mySheet['N1'] = 'Wrong Answer 3'
-mySheet['O1'] = 'Wrong Answer 4'
-mySheet['P1'] = 'Wrong Answer 5'
-mySheet['Q1'] = 'Wrong Answer 6'
-mySheet['R1'] = 'Wrong Answer 7'
-mySheet['S1'] = 'Wrong Answer 8'
-mySheet['T1'] = 'Correct Feedback'
-mySheet['U1'] = 'Incorrect Feedback'
-
-mySheet.column_dimensions['A'].width = 12
-mySheet.column_dimensions['B'].width = 10
-mySheet.column_dimensions['C'].width = 13
-mySheet.column_dimensions['D'].width = 13
-mySheet.column_dimensions['E'].width = 13
-mySheet.column_dimensions['F'].width = 13
-mySheet.column_dimensions['G'].width = 13
-mySheet.column_dimensions['H'].width = 13
-mySheet.column_dimensions['I'].width = 13
-mySheet.column_dimensions['J'].width = 13
-mySheet.column_dimensions['K'].width = 13
-mySheet.column_dimensions['L'].width = 14
-mySheet.column_dimensions['M'].width = 14
-mySheet.column_dimensions['N'].width = 14
-mySheet.column_dimensions['O'].width = 14
-mySheet.column_dimensions['P'].width = 14
-mySheet.column_dimensions['Q'].width = 14
-mySheet.column_dimensions['R'].width = 14
-mySheet.column_dimensions['S'].width = 14
-mySheet.column_dimensions['T'].width = 16
-mySheet.column_dimensions['U'].width = 18
-
-mySheet['A1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['B1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['C1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['D1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['E1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['F1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['G1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['H1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['I1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['J1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['K1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['L1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['M1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['N1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['O1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['P1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['Q1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['R1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['S1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['T1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-mySheet['U1'].fill = PatternFill(start_color="ff0000", fill_type="solid")
-# Thanks to user "crifan" on Stack Overflow for the right side of the equals sign of these ^ lines of code
-# https://stackoverflow.com/questions/35918504/adding-a-background-color-to-cell-openpyxl
 
 
-wb1.save(filepath)
+def create_file():
 
+    wb = openpyxl.Workbook()
+    filepath = "C:/Users/EvanS/Desktop/test.xlsx"
+    wb.save(filepath)
 
+    wb1 = openpyxl.load_workbook(filepath)
 
+    mySheet = wb1['Sheet']
 
+    mySheet['A1'] = 'Course Name'
+    mySheet['B1'] = 'Test Name'
+    mySheet['C1'] = 'Question Text'
+    mySheet['D1'] = 'Right Answer 1'
+    mySheet['E1'] = 'Right Answer 2'
+    mySheet['F1'] = 'Right Answer 3'
+    mySheet['G1'] = 'Right Answer 4'
+    mySheet['H1'] = 'Right Answer 5'
+    mySheet['I1'] = 'Right Answer 6'
+    mySheet['J1'] = 'Right Answer 7'
+    mySheet['K1'] = 'Right Answer 8'
+    mySheet['L1'] = 'Wrong Answer 1'
+    mySheet['M1'] = 'Wrong Answer 2'
+    mySheet['N1'] = 'Wrong Answer 3'
+    mySheet['O1'] = 'Wrong Answer 4'
+    mySheet['P1'] = 'Wrong Answer 5'
+    mySheet['Q1'] = 'Wrong Answer 6'
+    mySheet['R1'] = 'Wrong Answer 7'
+    mySheet['S1'] = 'Wrong Answer 8'
+    mySheet['T1'] = 'Correct Feedback'
+    mySheet['U1'] = 'Incorrect Feedback'
 
+    mySheet.column_dimensions['A'].width = 18
+    mySheet.column_dimensions['B'].width = 16
+    mySheet.column_dimensions['C'].width = 19
+    mySheet.column_dimensions['D'].width = 19
+    mySheet.column_dimensions['E'].width = 19
+    mySheet.column_dimensions['F'].width = 19
+    mySheet.column_dimensions['G'].width = 19
+    mySheet.column_dimensions['H'].width = 19
+    mySheet.column_dimensions['I'].width = 19
+    mySheet.column_dimensions['J'].width = 19
+    mySheet.column_dimensions['K'].width = 19
+    mySheet.column_dimensions['L'].width = 20
+    mySheet.column_dimensions['M'].width = 20
+    mySheet.column_dimensions['N'].width = 20
+    mySheet.column_dimensions['O'].width = 20
+    mySheet.column_dimensions['P'].width = 20
+    mySheet.column_dimensions['Q'].width = 20
+    mySheet.column_dimensions['R'].width = 20
+    mySheet.column_dimensions['S'].width = 20
+    mySheet.column_dimensions['T'].width = 22
+    mySheet.column_dimensions['U'].width = 24
+
+    thin = Side(border_style="thick", color="303030")
+    letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U"]
+    for x in letters:
+        column = x + "1"
+        mySheet[column].border = Border(top=thin, left=thin, right=thin, bottom=thin)
+        mySheet[column].font = Font(size=14, bold=True)
+        mySheet[column].alignment = Alignment(horizontal="center")
+        mySheet[column].fill = PatternFill(start_color="00ffcc", fill_type="solid")
+        # Thanks to user "crifan" on Stack Overflow for the right side of the assignment operator on the above line
+        # https://stackoverflow.com/questions/35918504/adding-a-background-color-to-cell-openpyxl
+
+    wb1.save(filepath)
--- main.py
+++ main.py
@@ -1,60 +1,87 @@
-import os #This is for the find function
+import os  # This is for the find function
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+import excelTest
 
 
-def find(name, path): # This is so the program can search for where the webdriver file is on the user's computer
+def find(name, path):  # This is so the program can search for where the webdriver file is on the user's computer
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
-        #else:
-         #   raise Exception("chromedriver.exe could not be found")
 
 
-# driver = webdriver.Chrome('C:/Users/EvanS/Downloads/chromedriver_win32/chromedriver.exe')
-# This ^^^ is so I don't forget where I have the webdriver on my computer
+def nav(  # reminder to future me to pass input here to determine which option was chosen and thus which function to
+        # call next from nav, taking care of the problem of driver being a local variable
+         ):
+    driver = webdriver.Chrome(find("chromedriver.exe", "C:/Users"))
+    # driver = webdriver.Chrome('C:/Users/EvanS/Downloads/chromedriver_win32/chromedriver.exe')
+    # This ^^^ is so I don't forget where I have the webdriver on my computer
+
+    # Reads in my login credentials. This will have to be changed later.
+    with open("C:/Users/EvanS/Documents/College/Capstone/credentials.txt") as f:
+        name = f.readline()
+        word = f.readline()
+        f.close()
+
+    driver.get('https://bb.nsuok.edu')
+    driver.maximize_window()
+
+    driver.find_element_by_id('username').send_keys(name)
+    driver.find_element_by_id('password').send_keys(word)
+    driver.find_element_by_name('submit').click()
+
+    try:
+        WebDriverWait(driver, .001).until(ec.url_matches('https://bb.nsuok.edu/ultra'))
+    except TimeoutError:
+        print('took too  long')
+
+    driver.implicitly_wait(20)
+    driver.find_element_by_link_text("Courses").click()
+    WebDriverWait(driver, 10).until(ec.url_matches('https://bb.nsuok.edu/ultra/course'))
+    driver.implicitly_wait(60)
+
+    driver.find_element_by_link_text("Bekkering-Capstone-Sandbox").click()
+
+    # driver.implicitly_wait(60)
+    # driver.find_element_by_link_text("Content").click()
+
+    driver.implicitly_wait(60)
+
+    element = WebDriverWait(driver, .001).until(ec.presence_of_element_located((By.XPATH, '//*[@id="paletteItem:_1135693_1"]/a')))
+    element.click()
+
+    # driver.find_element_by_xpath('//*[@id="paletteItem:_1135693_1"]/a').click()
 
 
-driver = webdriver.Chrome(find("chromedriver.exe", "C:/Users"))
+def main():
+    option = ""
+    while True:
+        print("1: Create Test File\n")
+        print("2: Upload Test\n")
+        print("3: Upload Test Pool\n")
+        print("4: Create Test From Existing Pools\n")
+        print("q: Quit")
+        option = input("Select Option:")
 
-# Reads in my login credentials. This will have to be changed later.
-with open("C:/Users/EvanS/Documents/College/Capstone/credentials.txt") as f:
-    name = f.readline()
-    word = f.readline()
-    f.close()
+        if option == "1":
+            excelTest.create_file()
+        if option == "2":
+            nav()
+        if option == "3":
+            nav()
+        if option == "4":
+            nav()
+        if option == "q":
+            break
 
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
-driver.get('https://bb.nsuok.edu')
-driver.maximize_window()
+if __name__ == "__main__":
+    main()
 
-driver.find_element_by_id('username').send_keys(name)
-driver.find_element_by_id('password').send_keys(word)
-driver.find_element_by_name('submit').click()
 
-try:
-    WebDriverWait(driver, .001).until(EC.url_matches('https://bb.nsuok.edu/ultra'))
-except TimeoutError:
-    print('took too  long')
-
-driver.implicitly_wait(20)
-driver.find_element_by_link_text("Courses").click()
-WebDriverWait(driver, 10).until(EC.url_matches('https://bb.nsuok.edu/ultra/course'))
-driver.implicitly_wait(60)
-
-driver.find_element_by_link_text("Bekkering-Capstone-Sandbox").click()
-
-#driver.implicitly_wait(60)
-#driver.find_element_by_link_text("Content").click()
-
-driver.implicitly_wait(60)
-
-element = WebDriverWait(driver, .001).until(EC.presence_of_element_located((By.XPATH, '//*[@id="paletteItem:_1135693_1"]/a')))
-element.click()
-
-#driver.find_element_by_xpath('//*[@id="paletteItem:_1135693_1"]/a').click()
 
 
 
